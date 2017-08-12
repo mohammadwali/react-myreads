64771d62720c586c48f45152b4d872260e9b541d
Mohammad Wali <mohdwali@live.com>
2017-08-12 22:46:06 +0530

    Installing Underscore and loading book shelves

---
+++ src/BookShelf.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+
+
+class BookShelf extends Component {
+
+
+    render() {
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{ this.props.title }</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+
+                        {
+                            this.props.books.map((book) => {
+
+                                return (
+                                    <li key={book.id}>
+                                        <div className="book">
+                                            <div className="book-top">
+                                                <div className="book-cover" style={{
+                                                    width: 128,
+                                                    height: 188,
+                                                    backgroundImage: 'url(' + book.imageLinks.thumbnail + ')'
+                                                }}/>
+                                                <div className="book-shelf-changer">
+                                                    <select>
+                                                        <option value="none" disabled>Move to...</option>
+                                                        <option value="currentlyReading">Currently Reading</option>
+                                                        <option value="wantToRead">Want to Read</option>
+                                                        <option value="read">Read</option>
+                                                        <option value="none">None</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div className="book-title">{ book.title }</div>
+                                            <div className="book-authors">{ book.authors.join(", ") }</div>
+                                        </div>
+                                    </li>
+                                )
+
+                            })
+                        }
+
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+
+
+}
+
+export default BookShelf
